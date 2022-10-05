b2592005baf9ec5119a436cd18e9890b5f8f8f86
Tereshchenko Denis <tereshchenkodenis@Tereshchenkos-MacBook-Air.local>
2022-10-05 10:37:38 +0300

    delete duplicates

--- zadacha1.py
+++
@@ -1,8 +0,0 @@
-def diagonalSum(mat):
-    sum = 0
-    for i in range(len(mat)):
-        for j in range(len(mat[i])):
-            if i == j or (i + j + 1) == len(mat):
-                sum += mat[i][j]
-    return sum
-                
