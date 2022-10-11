6471f1807536eefc83ddd7912e38355175ac2fef
dotereschenko <113333350+dotereschenko@users.noreply.github.com>
2022-10-11 22:47:26 +0300

    Update zadacha3.py

--- assignment_1/zadacha3.py
+++ assignment_1/zadacha3.py
@@ -1,46 +1,9 @@
-def merge(s, l):
-    arr = list()
-    pl = 1
-    pr = 0
-    left = s[:l]  
-    right = s[l:] 
-    print(left, right)
-    while len(left) + 1 > pl and len(right) > pr:
-        if abs(left[-pl]) <= right[pr]:
-            arr.append(abs(left[-pl]) ** 2)
-            pl += 1
-        else:
-            arr.append(right[pr] ** 2)
-            pr += 1
-    while len(left) + 1 > pl:
-        arr.append(abs(left[-pl]) ** 2)
-        pl += 1
-    while len(right) > pr:
-        arr.append(right[pr] ** 2)
-        pr += 1
-    return arr
-   
-def check(mid):
-    if s[mid] > 0:
-        return True
-    else:
-        return False
-    pass
+def squares(s):
 
+    i = 0
+    while (i < len(s) and s[i] < 0):
+        i += 1
+    s1 = [s[cur] ** 2 for cur in range(i - 1 , -1, -1)]
+    s2 = [s[cur] ** 2 for cur in range(i, len(s))]
 
-def main():
-    for i in range(len(s)):
-        l = 0
-        r = len(s)
-        while l < r:
-            mid = (l + r) // 2
-            if check(mid):  # if positive
-                r = mid - 1
-            else:
-                l = mid + 1
-    print(l)
-    return merge(s, l)
-
-
-if __name__ == '__main__':
-    print(main())
+    return merge(s1, s2)
